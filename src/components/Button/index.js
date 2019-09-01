8dcef92719b917d63c6f08a2cc4dab4eb17bc853
Daniel Santos <daniels11.deev@gmail.com>
2019-09-01 01:21:36 -0300

    Melhorando o componente Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,16 @@ import React from 'react';
 import './styles.css';
 
 export default (props) => {
+	let classes = "button ";
+	classes += props.operation ? 'operation' : '';
+	classes += props.double ? 'double' : '';
+	classes += props.triple ? 'triple' : '';
+
 	return (
-		<button className='button'>{props.label}</button>
+		<button 
+		onClick= {(e) => props.click && props.click(props.label)}
+		className={classes}>
+			{props.label}
+		</button>
 	);
 }
